a574d0d82c5f165c25bd4c432cc75ff12ebf1de1
tam <tam.vo@devplus.edu.vn>
2023-08-08 16:29:41 +0700

    create code base

---
+++ src/app.module.ts
@@ -0,0 +1,34 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+import * as path from 'path'; // Import module path
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('PG_HOST'),
+        port: configService.get('PG_PORT'),
+        username: configService.get('PG_USER'),
+        password: configService.get('PG_PASSWORD'),
+        database: configService.get('PG_DB'),
+
+        entities:  [path.join(__dirname, '**', '*.entity{.ts,.js}')],
+
+
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+     
+    }),
+
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
