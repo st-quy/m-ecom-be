de0466c57348b9868c10879bcaeb6042c30dfcca
tam <tam.vo@devplus.edu.vn>
2023-08-15 09:00:10 +0700

    dpm-create-CategoryModule

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Put } from '@nestjs/common';
+import { CategoryService } from './category.service';
+import { Category } from './entities';
 
 @Controller('category')
-export class CategoryController {}
+export class CategoryController {
+
+constructor(private readonly categoryService: CategoryService){}
+
+@Get()
+  async findAll() {
+    return this.categoryService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: number) {
+    return this.categoryService.findOne(id);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: number, @Body() category: Category) {
+    return this.categoryService.update(id, category);
+  }
+
+  @Delete(':id')
+  async deleteCategory(@Param('id') id: number): Promise<void> {
+    await this.categoryService. remove(id);
+  }
+}
--- src/modules/category/category.service.ts
+++ src/modules/category/category.service.ts
@@ -1,4 +1,75 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { Category } from './entities';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Products } from '../products/entities';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  constructor(
+    @InjectRepository(Category)
+    private readonly categoryRepository: Repository<Category>,
+    @InjectRepository(Category)
+    private readonly productRepository: Repository<Products>,
+  ) { }
+
+  async findAll(): Promise<Category[]> {
+    return this.categoryRepository.find();
+  }
+
+  async findOne(id: number): Promise<Category> {
+    const category = await this.categoryRepository.findOne({ where: { id } });
+    if (!category) {
+      throw new NotFoundException(`Category with id ${id} not found`);
+    }
+    return category;
+  }
+
+  async update(id: number, category: Category): Promise<Category> {
+    const existingCategory = await this.categoryRepository.findOne({ where: { id } });
+    if (!existingCategory) {
+      throw new NotFoundException(`Category with id ${id} not found`);
+    }
+
+    // Update the category fields based on the received data
+    existingCategory.category_name = category.category_name;
+
+    return this.categoryRepository.save(existingCategory);
+  }
+
+  async remove(id: number): Promise<void> {
+    
+    const category = await this.categoryRepository
+      .createQueryBuilder('category')
+      .leftJoinAndSelect('category.products', 'product')
+      .where('category.id = :id', { id })
+      .getOne();
+  
+    console.log(category, 'aaaa');
+  
+    if (!category) {
+      throw new NotFoundException(`Category with id ${id} not found`);
+    }
+
+    const hasActiveProducts = category.products.some(product => product.status === 'active');
+
+    if (hasActiveProducts) {
+      throw new BadRequestException('Cannot delete category with active products');
+    }
+  
+    category.delete_at = new Date();
+    await this.categoryRepository.save(category);
+  
+    // Soft delete related products
+    const productIds = category.products.map(product => product.id);
+  
+    await this.productRepository
+      .createQueryBuilder()
+      .update(Products)
+      .set({ delete_At: new Date() }) // Soft delete marker
+      .where('id IN (:...productIds)', { productIds }) // Use IN operator with product IDs
+      .execute();
+  }
+ 
+}
+
