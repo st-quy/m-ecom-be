164cf98fa519e8426c9f0808a949754992626162
biennguyen110821 <bien.nguyen@devplus.edu.vn>
2023-08-28 16:03:23 +0700

    update GoogleOauth

--- src/modules/auth/strategies/google.strategy.ts
+++ src/modules/auth/strategies/google.strategy.ts
@@ -1,11 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, VerifyCallback } from 'passport-google-oauth20';
 import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
-  constructor(private configService: ConfigService) {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {
     super({
       clientID: configService.get('GOOGLE_CLIENT_ID'),
       clientSecret: configService.get('GOOGLE_CLIENT_SECRET'),
@@ -16,6 +20,26 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
   }
 
   async validate(request: any, accessToken: string, refreshToken: string, profile: any, done: VerifyCallback) {
-    done(null, profile);
+    const user = {
+      name: profile.displayName,
+      refreshToken: refreshToken,
+    };
+
+    done(null, user);
+  }
+
+  async generateAccessToken(user: any): Promise<string> {
+    const payload = {
+      name: user.name,
+      role: 'user', // Default role for Google login
+      refreshToken: user.refreshToken,
+    };
+
+    const generatedAccessToken = await this.jwtService.signAsync(payload, {
+      secret: this.configService.get<string>('ACCESS_TOKEN_SECRET'),
+      expiresIn: this.configService.get<string>('EXP_IN_ACCESS_TOKEN'),
+    });
+
+    return generatedAccessToken;
   }
 }
--- src/modules/firebase/firebase.controller.ts
+++ src/modules/firebase/firebase.controller.ts
@@ -1,24 +1,40 @@
 import { Controller, Get, Req, UnauthorizedException, UseGuards } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
 import * as admin from 'firebase-admin';
 
-@Controller('')
+@Controller()
 export class FirebaseController {
-  constructor() {}
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Get('login/google')
   @UseGuards(AuthGuard('google'))
   async googleLogin() {}
 
-  @Get('login/google/callback')
+   @Get('login/google/callback')
   @UseGuards(AuthGuard('google'))
   async googleLoginCallback(@Req() req: Request) {
-    const id = req.user['id'];
-    if (!id) {
-      throw new UnauthorizedException('User ID not found.');
-    }
-    const accessToken = await admin.auth().createCustomToken(id);
+    const user = req.user as any;
+    const accessToken = await this.generateAccessToken(user);
     return { accessToken };
+  }
+
+  private async generateAccessToken(user: any): Promise<string> {
+    const payload = {
+      name: user.name,
+      role: 'user', // Default role for Google login
+      refreshToken: user.refreshToken,
+    };
+
+    const generatedAccessToken = await this.jwtService.signAsync(payload, {
+      secret: this.configService.get<string>('ACCESS_TOKEN_SECRET'),
+      expiresIn: this.configService.get<string>('EXP_IN_ACCESS_TOKEN'),
+    });
+    return generatedAccessToken;
   }
 }
